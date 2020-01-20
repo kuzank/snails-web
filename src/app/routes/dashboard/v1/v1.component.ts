e8923b207de5f6fc0e5f87bf5e432dd62848b6e5
kuzan <754109648@qq.com>
2020-01-20 14:51:47 +0800

    首页修改

--- src/app/routes/dashboard/v1/v1.component.ts
+++ src/app/routes/dashboard/v1/v1.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
+import { zip } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { RouterHelper } from '@core/helper/RouterHelper';
 
 @Component({
   selector: 'app-dashboard-v1',
@@ -7,57 +10,73 @@ import { _HttpClient } from '@delon/theme';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardV1Component implements OnInit {
-  todoData: any[] = [
-    {
-      completed: true,
-      avatar: '1',
-      name: '苏先生',
-      content: `请告诉我，我应该说点什么好？`,
-    },
-    {
-      completed: false,
-      avatar: '2',
-      name: 'はなさき',
-      content: `ハルカソラトキヘダツヒカリ`,
-    },
-    {
-      completed: false,
-      avatar: '3',
-      name: 'cipchk',
-      content: `this world was never meant for one as beautiful as you.`,
-    },
-    {
-      completed: false,
-      avatar: '4',
-      name: 'Kent',
-      content: `my heart is beating with hers`,
-    },
-    {
-      completed: false,
-      avatar: '5',
-      name: 'Are you',
-      content: `They always said that I love beautiful girl than my friends`,
-    },
-    {
-      completed: false,
-      avatar: '6',
-      name: 'Forever',
-      content: `Walking through green fields ，sunshine in my eyes.`,
-    },
-  ];
 
-  webSite: any[];
+  onlinePersons = [];
+  personNum = 0;
+  exceptioNum = 0;
+  httpLogNum = 0;
+
   salesData: any[];
   offlineChartData: any[];
 
-  constructor(private http: _HttpClient, private cdr: ChangeDetectorRef) {}
+  constructor(private http: _HttpClient,
+              private routerHelper: RouterHelper,
+              private cdr: ChangeDetectorRef) {
+  }
 
   ngOnInit() {
-    // this.http.get('/chart').subscribe((res: any) => {
-    //   this.webSite = res.visitData.slice(0, 10);
-    //   this.salesData = res.salesData;
-    //   this.offlineChartData = res.offlineChartData;
-    //   this.cdr.detectChanges();
-    // });
+    let param = {
+      pageNumber: 0,
+      pageSize: 100000,
+    };
+    zip(
+      this.http.post(`/person/online`),
+      this.http.get('/person/num'),
+      this.http.get('/exceptio/num'),
+      this.http.get('/httpLog/num'),
+    ).pipe(
+      // 接收其他拦截器后产生的异常消息
+      catchError(([online, personNum, exceptioNum, httpLogNum]) => {
+        return [online, personNum, exceptioNum, httpLogNum];
+      }),
+    ).subscribe(([online, personNum, exceptioNum, httpLogNum]) => {
+
+        this.onlinePersons = online.data as [];
+        this.personNum = personNum.data;
+        this.exceptioNum = exceptioNum.data;
+        this.httpLogNum = httpLogNum.data;
+
+        // 假数据
+        this.mockData();
+
+        this.cdr.detectChanges();
+      },
+    );
+
+  }
+
+  mockData() {
+    let _salesData = [];
+    for (let i = 0; i < 12; i += 1) {
+      _salesData.push({
+        x: `${i + 1}月`,
+        y: Math.floor(Math.random() * 1000) + 200,
+      });
+    }
+    this.salesData = _salesData;
+
+    const _offlineChartData: any[] = [];
+    for (let i = 0; i < 20; i += 1) {
+      _offlineChartData.push({
+        x: new Date().getTime() + 1000 * 60 * 30 * i,
+        y1: Math.floor(Math.random() * 100) + 10,
+        y2: 0,
+      });
+    }
+    this.offlineChartData = _offlineChartData;
+  }
+
+  goTo(url: string) {
+    this.routerHelper.goTo(url, false);
   }
 }
