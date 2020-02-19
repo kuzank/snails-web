cc843ea3a912eede7a40c4d881c0b5a254439782
kuzank <754109648@qq.com>
2020-02-19 20:03:38 +0800

    添加自定义图形组件，三条曲线；添加24小时、29天图形例子

--- src/app/routes/dashboard/v2/v2.component.ts
+++ src/app/routes/dashboard/v2/v2.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-v2',
@@ -11,212 +11,37 @@ export class DashboardV2Component implements OnInit {
   constructor() {
   }
 
+  data1 = [];
+  data2 = [];
+
   ngOnInit() {
-
+    this.initG2Timeline3Data1();
+    this.initG2Timeline3Data2();
   }
 
-  render1(el: ElementRef) {
-    fetch('https://g2.antv.vision/zh/examples/data/siteUV.json')
-      .then(res => res.json())
-      .then(data => {
-        const chart = new G2.Chart({
-          container: el.nativeElement,
-          forceFit: true,
-          height: 400,
-          padding: [20, 90, 50, 50],
-        });
-        const ds = new DataSet();
-        const dv = ds.createView().source(data);
-        dv.transform({
-          type: 'map',
-          callback: function callback(row) {
-            const times = row.Time.split(' ');
-            row.date = times[0];
-            row.time = times[1];
-            return row;
-          },
-        });
-        chart.axis('time', {
-          label: {
-            textStyle: {
-              fill: '#aaaaaa',
-            },
-          },
-        });
-        chart.axis('Count', {
-          label: {
-            textStyle: {
-              fill: '#aaaaaa',
-            },
-            formatter: text => {
-              return text.replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
-            },
-          },
-        });
-
-        chart.tooltip({
-          crosshairs: false,
-        });
-
-        chart.legend({
-          attachLast: true,
-        });
-
-        chart.source(dv, {
-          time: {
-            tickCount: 24,
-          },
-          date: {
-            type: 'cat',
-          },
-        });
-
-        chart.line().position('time*Count').color('date', ['#d9d9d9', '#1890ff']);
-
-        chart.render();
-      });
-
-  }
-
-  render2(el: ElementRef) {
-    fetch('https:/g2.antv.vision/zh/examples/data/diamond.json')
-      .then(res => res.json())
-      .then(data => {
-        const chart = new G2.Chart({
-          container: el.nativeElement,
-          forceFit: true,
-          height: 400,
-          padding: 'auto',
-        });
-        chart.scale({
-          x: {
-            alias: 'depth',
-            min: 50,
-            max: 70,
-            sync: true,
-          },
-          y: {
-            alias: '概率密度分布',
-            sync: true,
-          },
-        });
-
-        [
-          'boxcar',
-          'cosine',
-          'epanechnikov',
-          'gaussian',
-          'quartic',
-          'triangular',
-          'tricube',
-          'triweight',
-          'uniform',
-        ].forEach((method, i) => {
-          const dv = new DataSet.View().source(data);
-          dv.transform({
-            type: 'kernel-smooth.regression',
-            method,
-            field: 'depth',
-            extent: [50, 70],
-          });
-
-          const view = chart.view();
-          !!i && view.axis(false);
-          view.source(dv);
-          view.line()
-            .position('x*y')
-            .color(G2.Global.colors_16[i]);
-        });
-
-        chart.render();
-      });
-  }
-
-  render3(el: ElementRef) {
-
-    let tdata = [];
-    for (let i = 1; i <= 24; i++) {
-      tdata.push({
-        type: '方案一',
-        Count: i,
-        Time: '2020/01/17 ' + i + ':00',
-      });
-      tdata.push({
-        type: '方案二',
-        Count: i + 4,
-        Time: '2020/01/17 ' + i + ':00',
-      });
-      tdata.push({
-        type: '方案三',
-        Count: i + 7,
-        Time: '2020/01/17 ' + i + ':00',
+  initG2Timeline3Data1() {
+    let data = [];
+    for (let i = 0; i <= 24; i++) {
+      data.push({
+        x: (new Date('2020-02-19 00:00:00').getTime()) + (60 * 60 * 1000 * i),
+        y1: Math.floor(Math.random() * 50),
+        y2: Math.floor(Math.random() * 50),
+        y3: Math.floor(Math.random() * 50),
       });
     }
-
-    const chart = new G2.Chart({
-      container: el.nativeElement,
-      forceFit: true,
-      height: 400,
-      padding: [20, 90, 50, 50],
-    });
-    const ds = new DataSet();
-    const dv = ds.createView().source(tdata);
-    dv.transform({
-      type: 'map',
-      callback: function callback(row) {
-        const times = row.Time.split(' ');
-        row.date = times[0];
-        row.time = times[1];
-        return row;
-      },
-    });
-    chart.axis('time', {
-      label: {
-        textStyle: {
-          fill: '#aaaaaa',
-        },
-      },
-    });
-    chart.axis('Count', {
-      label: {
-        textStyle: {
-          fill: '#aaaaaa',
-        },
-        formatter: text => {
-          return text.replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
-        },
-      },
-    });
-
-    chart.source(dv, {
-      time: {
-        tickCount: 24,
-      },
-      date: {
-        type: 'cat',
-      },
-    });
-
-    chart.tooltip({
-      crosshairs: false,
-    });
-    chart.legend({
-      attachLast: true,
-    });
-
-    chart.line().position('time*Count').color('type', (type) => { // 通过回调函数
-      if (type === '方案一') {
-        return 'red';
-      }
-      if (type === '方案二') {
-        return 'green';
-      }
-      return 'blue';
-    });
-
-    chart.render();
-
-
+    this.data1 = data;
   }
 
+  initG2Timeline3Data2() {
+    let data = [];
+    for (let i = 1; i < 29; i++) {
+      data.push({
+        x: new Date('2020-02-' + i),
+        y1: Math.floor(Math.random() * 50) + 15,
+        y2: Math.floor(Math.random() * 50) + 15,
+        y3: Math.floor(Math.random() * 50) + 15,
+      });
+    }
+    this.data2 = data;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 // delon
 import { AlainThemeModule } from '@delon/theme';
@@ -10,19 +10,23 @@ import { DelonACLModule } from '@delon/acl';
 import { DelonFormModule } from '@delon/form';
 // i18n
 import { TranslateModule } from '@ngx-translate/core';
-
 // #region third libs
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { CountdownModule } from 'ngx-countdown';
 import { UEditorModule } from 'ngx-ueditor';
 import { NgxTinymceModule } from 'ngx-tinymce';
+import { timeline3Component } from '@shared/chat/timeline3/timeline3.component';
+import { DelonUtilModule } from '@delon/util';
 
 const THIRDMODULES = [NgZorroAntdModule, CountdownModule, UEditorModule, NgxTinymceModule];
 // #endregion
 
 // #region your componets & directives
-const COMPONENTS = [];
+const COMPONENTS = [
+  timeline3Component,
+];
 const DIRECTIVES = [];
+
 // #endregion
 
 @NgModule({
@@ -38,6 +42,7 @@ const DIRECTIVES = [];
     DelonFormModule,
     // third libs
     ...THIRDMODULES,
+    DelonUtilModule,
   ],
   declarations: [
     // your components
@@ -63,4 +68,5 @@ const DIRECTIVES = [];
     ...DIRECTIVES,
   ],
 })
-export class SharedModule {}
+export class SharedModule {
+}
